8ae3fd9fb05950d855c68f8aebbecb4a16db3752
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2022-12-12 13:10:42 +0200

    ten

--- js/task-10.js
+++ js/task-10.js
@@ -5,13 +5,40 @@ function getRandomHexColor() {
 const refs = {
   input: document.querySelector('#controls input'),
   boxes: document.querySelector('#boxes'),
-
+  createBtn: document.querySelector('#controls button'),
+  destroyBtn: document.querySelector('[data-action="destroy"]'),
+  //createBtn: document.querySelector('[data-action="create"]'),
 }
-function moreBoxes(event) {
-  const box1 = document.createElement('div');
+//destroyBtn = refs.createBtn.nextElementSibling;
+//console.log(destroyBtn);
 
+const frag = document.createDocumentFragment();
+
+refs.createBtn.addEventListener('click', createBoxes);
+//refs.destroyBtn.addEventListener('click', destroyAll);
+
+const num = Number(refs.input.value)
+console.log(num);
+
+function createBoxes(num) {
+  refs.boxes.classList.add('image--item');
+  for (let i = 0; i < Number(refs.input.value); i += 1) {
+    console.log('hi');
+    const box = '<div height=100px ></div >';
+    //box.style.color = getRandomHexColor();
+    console.log(box);
+    frag.append(box);
+    console.log(frag);
+}
 };
 
-for (let i = 0; i < refs.input.value; i += 1) {
-  
-}
+refs.boxes.insertAdjacentHTML('beforeend', frag);
+
+function destroyAll(event) {
+  console.log('hi');
+  refs.boxes = '';
+};
+
+
+
+
